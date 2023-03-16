de483ab799e9872cc7e88c1aeb467d4fd669878e
Ivan <ivanaires32@gmail.com>
2023-03-16 00:01:18 -0300

    correção menu

--- src/components/TopFooter/Fixos.js
+++ src/components/TopFooter/Fixos.js
@@ -4,17 +4,19 @@ import { Link } from "react-router-dom";
 export default function Fixos({ userDados }) {
     return (
         <>
-            <Header>
+            <Header data-test="header">
                 <h1>TrackIt</h1>
                 <img src={userDados.image} />
             </Header>
 
-            <Footer>
-                <Link to={"/habitos"}>
+            <Footer data-test="menu">
+                <Link data-test="habit-link" to={"/habitos"}>
                     <h2>Hábitos</h2>
                 </Link>
-                <div>Hoje</div>
-                <h2>Histórico</h2>
+                <div data-test="today-link">Hoje</div>
+                <Link data-test="history-link" to={"/historico"}>
+                    <h2>Histórico</h2>
+                </Link>
             </Footer>
         </>
     )
