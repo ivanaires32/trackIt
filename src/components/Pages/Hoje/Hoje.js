17ba17b1bcb6a44e06dfa373b743aa46e61c2571
Ivan <ivanaires32@gmail.com>
2023-03-19 22:13:27 -0300

    Dia corrigido para pt-br

--- src/components/Pages/Hoje/Hoje.js
+++ src/components/Pages/Hoje/Hoje.js
@@ -14,55 +14,58 @@ export default function Hoje() {
     const [percentagem, setPercentagem] = useState()
     const [num, setNum] = useState(0)
     const [atual, setAtual] = useState(0)
-    const [inicial, setInicial] = useState(true)
-    const [igual, setIgual] = useState(false)
     const vazio = {}
     const dados = useContext(Dados)
-    const d = dayjs().format('dddd - DD/MM')
-    let data = require('dayjs/plugin/customParseFormat')
-    dayjs.extend(data)
+    const [d, setD] = useState(dayjs().format('dddd'))
+    const mes = dayjs().format("DD/MM")
     const config = {
         headers: {
             "Authorization": `Bearer ${dados.userDados.token}`
         }
     }
 
+
     useEffect(() => {
         axios.get(`https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/today`, config)
             .then(res => {
                 setHabitos(res.data)
-                { habitos.map(() => setNum(0 + 1)) }
             })
             .catch(err => console.log(err.response.data))
-    }, [inicial])
+
+        if (d === "Sunday") {
+            setD("Domingo")
+        } else if (d === "Monday") {
+            setD("Segunda")
+        } else if (d === "Tuesday") {
+            setD("Terça")
+        } else if (d === "Wednesday") {
+            setD("Quarta")
+        } else if (d === "Thursday") {
+            setD("Quinta")
+        } else if (d === "Friday") {
+            setD("Sexta")
+        } else {
+            setD("Sábado")
+        }
+    }, [])
 
     useEffect(() => {
         setPercentagem(100 / habitos.length)
     }, [habitos])
 
-    function concluido(i, h) {
+    function concluido(i) {
         if (!check.includes(i)) {
             setCheck([...check, i])
             setNum(num + percentagem)
             axios.post(`https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/${i}/check`, vazio, config)
-                .then(() => {
-                    setAtual(atual + 1)
-                    setInicial(false)
-                    if (atual === h) {
-                        setIgual(true)
-                    }
-                })
                 .catch(err => console.log(err.response.data))
         } else {
             const newList = check.filter((h) => h !== i)
             setCheck(newList)
             setNum(num - percentagem)
-            setAtual(atual - 1)
             axios.post(`https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/${i}/uncheck`, vazio, config)
                 .catch(err => console.log(err.response))
         }
-
-
 
 
     }
@@ -70,7 +73,7 @@ export default function Hoje() {
         <Container>
             {TopFooter}
             <Topo color={num === 0 ? "#BABABA" : "#8FC549"}>
-                <h1 data-test="today">{d}</h1>
+                <h1 data-test="today">{d + " - " + mes}</h1>
                 <h2 data-test="today-counter">{check.length !== 0 ? `${num.toFixed(0)}% dos hábitos concluídos` : "Nem um habito concluido ainda"}</h2>
             </Topo>
 
@@ -78,13 +81,17 @@ export default function Hoje() {
                 <BoxContainer data-test="today-habit-container" key={h.id}>
                     <TituloHabito>
                         <h1 data-test="today-habit-name">{h.name}</h1>
-                        <h2 data-test="today-habit-sequence" >{`Sequência atual: `}<Span color={h.done ? "#8FC549" : "#666666"}>{`${inicial ? h.currentSequence : atual} dias`}</Span></h2>
+
+                        <h2 data-test="today-habit-sequence" >{`Sequência atual: `}
+                            <Span color={check.includes(h.id) ? "#8FC549" : "#666666"}>{
+                                `${check.includes(h.id) ? h.currentSequence + 1 : h.currentSequence} dias`}</Span></h2>
+
                         <h2 data-test="today-habit-record">{`Seu recorde: `}
-                            <Span color={h.done === true ? "#8FC549" : "#666666"}>
-                                {`${inicial === true && h.highestSequence > atual ? h.highestSequence : atual} dias`}</Span>
+                            <Span color={check.includes(h.id) ? "#8FC549" : "#666666"}>
+                                {`${check.includes(h.id) ? h.highestSequence + 1 : h.highestSequence} dias`}</Span>
                         </h2>
                     </TituloHabito>
-                    <Check data-test="today-habit-check-btn" background={check.includes(h.id) || h.done === true ? "#8FC549" : "#EBEBEB"} onClick={() => concluido(h.id, h.highestSequence)}><GoCheck /></Check>
+                    <Check data-test="today-habit-check-btn" background={check.includes(h.id) || h.done === true ? "#8FC549" : "#EBEBEB"} onClick={() => concluido(h.id)}><GoCheck /></Check>
                 </BoxContainer>
             ))
             }
